db825665486a6b259f882a01089a045754960ddf
Levy <levy.chang@cybertan.com.tw>
2018-01-16 09:52:20 +0800

    開始修改paratemer

--- main.c
+++ main.c
@@ -4,12 +4,21 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 #include "meshpos.h"
+#define ERR_EXIT(m) \
+    do { \
+        perror(m); \
+        exit(EXIT_FAILURE); \
+    } while(0)
 
 char *cgi = "-host 06:00:00:00:00:00"
             " -ab \"MAC: 06:01:22:00:00:01, RSSI: -38/-42\""
             " -ac \"MAC: 06:01:22:00:00:D3, RSSI: -43/-37\""
             " -bc \"MAC: 06:01:22:00:00:D3, RSSI: -51/-54\"";
+
+void handler(int, siginfo_t *, void *);
 
 int main(int argc, char *argv[])
 {
@@ -21,54 +30,73 @@ int main(int argc, char *argv[])
     waps[1] = init_wap();
     waps[2] = init_wap();
 
-    //find parameter
-    for(i=0;i<(unsigned int)argc;i++)
-        if(indexOf(argv[i],IN) != -1 && i+1 < (unsigned int)argc){
-            _argv = (char**)malloc(((PARAMETER_COUNT*2) + 1)  * sizeof(char*));
-            _argv[0] = (char*)malloc(strlen(argv[i+1]) + 1);
-            strcpy(_argv[0], argv[i+1]); //copy filename string
-            isfile = 1;
-            break;
-        }
+    //register siganl
+    struct sigaction act;
+    act.sa_sigaction = handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_SIGINFO;
 
-    if(isfile == 1){
-        check = readfile(_argv[0], &_argc, _argv); //read file
+    if (sigaction(2, &act, NULL) < 0) //use SIGRTMIN signal
+        ERR_EXIT("sigaction error");
 
-        if(check == 1){
-            release(&_argc, _argv);
-            print_file_error(_argv[0]);
-            return 0;
-        }else if(check == 2){
-            release(&_argc, _argv);
-            return 0;
-        }
+    while(1){
+        //wait signal
+        //pause();
+
+        //find parameter
+        for(i=0;i<(unsigned int)argc;i++)
+            if(indexOf(argv[i],IN) != -1 && i+1 < (unsigned int)argc){
+                _argv = (char**)malloc(((PARAMETER_COUNT*2) + 1)  * sizeof(char*));
+                _argv[0] = (char*)malloc(strlen(argv[i+1]) + 1);
+                strcpy(_argv[0], argv[i+1]); //copy filename string
+                isfile = 1;
+                break;
+            }
 
         //input waps array
-        if(argv_to_struct(_argc, _argv, waps)){
-            release(&_argc, _argv);
-            print_file_error(_argv[0]);
+        if(isfile == 1){
+            check = readfile(_argv[0], &_argc, _argv); //read file
+
+            if(check == 1){
+                release(&_argc, _argv);
+                print_file_error(_argv[0]);
+                return 0;
+            }else if(check == 2){
+                release(&_argc, _argv);
+                return 0;
+            }
+
+            if(argv_to_struct(_argc, _argv, waps)){
+                release(&_argc, _argv);
+                print_file_error(_argv[0]);
+                return 0;
+            }
+        }else{
+            printf("parameter error.\n./MeshMap -in filename\n");
             return 0;
         }
-    }else{
-        printf("parameter error.\n./MeshMap -in filename\n");
-        return 0;
+
+        //calc
+        rssi2dist(waps);
+        dist2coordinate(waps);
+
+        //output
+        printf("5G:\n");
+        for(i=0;i<3;i++)
+            printf("coor=%s=(%.2f,%.2f);\n", waps[i].bssid, waps[i].X,waps[i].Y);
+        for(i=0;i<3;i++)
+            printf("dist=%s-%s=%.1fm=%.f;\n", waps[i].bssid, waps[i].neighbor.link->bssid,
+                   waps[i].neighbor.distance, waps[i].neighbor.rssi_merge);
+
+        //release
+        if(isfile == 1)
+            release(&_argc, _argv);
     }
 
-    //calc
-    rssi2dist(waps);
-    dist2coordinate(waps);
-
-    //output
-    printf("5G:\n");
-    for(i=0;i<3;i++)
-        printf("coor=%s=(%.2f,%.2f);\n", waps[i].bssid, waps[i].X,waps[i].Y);
-    for(i=0;i<3;i++)
-        printf("dist=%s-%s=%.1fm=%.f;\n", waps[i].bssid, waps[i].neighbor.link->bssid,
-               waps[i].neighbor.distance, waps[i].neighbor.rssi_merge);
-
-    //release
-    if(isfile == 1)
-        release(&_argc, _argv);
-
     return 0;
+}
+
+void handler(int sig, siginfo_t *info, void *ctx)
+{
+    printf("recv a sig=%d data=%d\n", sig, info->si_value.sival_int);
 }
